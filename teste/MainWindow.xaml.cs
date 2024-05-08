5b541a8233f82fe458ad194245d3a4da81826fd0
luiz henrique <luiz_henrique_silva@outlook.com>
2024-05-08 20:45:47 -0300

    Add project files.

---
+++ teste/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGame
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			SetUpGame();
+
+		}
+
+		private void SetUpGame()
+		{
+			List<string> animalEmoji = new List<string>()
+			{
+			"ᓚᘏᗢ","ᓚᘏᗢ",
+			"(╯°□°）╯︵ ┻━┻","(╯°□°）╯︵ ┻━┻",
+			"(⌐■_■)","(⌐■_■)",
+			"(❁´◡`❁)","(❁´◡`❁)",
+			"ಠ_ಠ","ಠ_ಠ",
+			"ಥ_ಥ","ಥ_ಥ", //forgive me.
+			"(^///^)","(^///^)",
+			"(•_•)","(•_•)",
+			};
+			Random random = new Random();
+
+			foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+			{
+				int index = random.Next(animalEmoji.Count);
+				string nextEmoji = animalEmoji[index];
+				textBlock.Text = nextEmoji;
+				animalEmoji.RemoveAt(index);
+			}
+
+		}
+	}
+}
